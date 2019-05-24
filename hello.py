cba63b90b51e27a8d108c1baa4c75fbfa485e6ff
Vercaca <verca2510@iir.csie.ncku.edu.tw>
2019-05-24 21:46:01 +0800

    practicing

--- hello.py
+++ hello.py
@@ -1,16 +1,24 @@
 # hello.py
+from time import sleep
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "<h1>Hello World</h1>"
+    user_argent = request.headers.get("User-Agent")
+    user_name = request.args.get("name")
+    # return "<h1>Hello World</h1>"
+    return f"<p>Your browser is {user_argent}</p>" \
+        f"<p>Your name is {user_name}</p>"
+
 
 @app.route("/error")
 def error():
     raise RuntimeError
+
 
 @app.route("/users")
 def get_users():
@@ -21,5 +29,22 @@ def get_users():
     return resp
 
 
+@app.route("/user/<name>")
+def get_user_name(name):
+    return "<h1>Hello, {}!</h1>".format(name)
+
+
+@app.route("/user/<int:uid>")
+def get_user_id(uid):
+    if isinstance(uid, int):
+        return "<h1>Your ID: {}</h1>".format(uid)
+    return "<h1>ID should be int</h1>"
+
+
+@app.route("/user/<path:path>")
+def get_user_path(path):
+    return "<h1>Path: {}</h1>".format(path)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(threaded=True, debug=True)
